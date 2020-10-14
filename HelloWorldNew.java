f291003725435cbc746af53a53b514f982a2913f
Deepan Kumar <40297281+deepanuk@users.noreply.github.com>
2020-10-14 19:20:46 +0530

    Update HelloWorldNew.java
    modified for Git Webhooks testing

--- HelloWorldNew.java
+++ HelloWorldNew.java
@@ -4,6 +4,7 @@ public class JenkinsExample {
 
 	public static void main(String[] args) {
 		System.out.println("***** Hello All, This is my first Jenkins JAVA build ******");
+		System.out.println("***** Hello All, This is for GIT Webhooks trial ******");
 	}
 
 }
