9bd34a7f941d1e1c89681abbd4b8320fa8a8084e
Deepan Kumar <ukdeepankumar@gmail.com>
2020-10-14 18:25:35 +0530

    Adding Original file

---
+++ HelloWorldNew.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		System.out.println("***** Hello All, This is my first Jenkins JAVA build ******");
+	}
+
+}
